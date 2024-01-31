2322bc93b72aa6142fe38faa2255da4c2337c6b6
COOLICEBLAST <cooliceblast0904@gmail.com>
2024-01-31 08:26:58 +0100

    Mint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
+import { getAccount } from 'simple-web3';
 import './App.css';
+import { CONTRACT_ABI, CONTRACT_ADDRESS,GAS_LIMIT } from './config';
 
 function App() {
+  const[loading, setLoading] = useState(false);
+  const mintNFT = async () => {
+    try{
+      setLoading(true);
+      let ACCOUNT = await getAccount();
+      const contract  = new window.web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS)
+
+      await contract.methods.mint().send({
+        from:ACCOUNT,
+        gasLimit: GAS_LIMIT,
+        value: 0
+      })
+
+      setLoading(false)
+      alert('Yayy! minted a GoldenMan')
+    } catch (e) {
+      setLoading(false)
+      console.log(e)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    loading?
+    <div className='App'>
+        <h1>Minting</h1>
     </div>
+    : <div className='App'>
+        <h1>Mint my NFT</h1>
+        <button onClick={mintNFT}>Mint an NFT</button>
+      </div>
   );
 }
 
